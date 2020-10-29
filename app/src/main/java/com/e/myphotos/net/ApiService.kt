bd9bd37e3e36afb830b0096996fe0bb3cb12479d
RojoF <rojof83@gmail.com>
2020-10-29 19:32:15 +0100

    Añadir Compartir Card Clean Code

--- app/src/main/java/com/e/myphotos/net/ApiService.kt
+++ app/src/main/java/com/e/myphotos/net/ApiService.kt
@@ -8,7 +8,9 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET("?method=flickr.photos.search&format" +
-            "=json&nojsoncallback=1&api_key=55dd726c0b4d19c697782a9307e665db")
+            "=json&nojsoncallback=1&api_key=55dd726c0b4d19c697782a9307e665db" )
     suspend fun fetchImages(@Query(value = "text") searchText: String?): PhotosSearchResponse
+
+
 }
 
--- app/src/main/java/com/e/myphotos/ui/PhotoActivity.kt
+++ app/src/main/java/com/e/myphotos/ui/PhotoActivity.kt
@@ -1,13 +1,26 @@
 package com.e.myphotos.ui
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import com.e.myphotos.R
 import com.e.myphotos.databinding.ActivityPhotoBinding
 import com.e.myphotos.domain.Photo
-import com.github.chrisbanes.photoview.PhotoView
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_photo.*
+import kotlinx.android.synthetic.main.card.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
 
 class PhotoActivity : AppCompatActivity() {
 
@@ -16,19 +29,74 @@ class PhotoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_photo)
-        val photo = intent.getSerializableExtra("photo") as? Photo
+        binding.apply {
+            val photoView = imageViewfull
+            val photo = intent.getSerializableExtra("photo") as? Photo
 
-        val photoView = binding.imageViewfull
-        photoView.isZoomable
-        photoView.scaleType
-        if (photo != null) {
-            binding.titleFullphoto.text = photo.title
-            binding.idFullphoto.text = photo.id
+            ButtonShare.setOnClickListener {
+                comparteView(viewShare = card)
+
+            }
+            photoView.isZoomable
+            photoView.scaleType
+
+            if (photo != null) {
+                titleFullphoto.text = photo.title
+                idFullphoto.text = photo.id
+            }
+            Picasso.get().load(photo?.url)
+                .resize(IMAGE_SIDE_PX, IMAGE_SIDE_PX)
+                .centerCrop()
+                .into(imageViewfull)
         }
-        Picasso.get().load(photo?.url)
-            .resize(IMAGE_SIDE_PX, IMAGE_SIDE_PX)
-            .centerCrop()
-            .into(binding.imageViewfull)
+    }
+
+
+    private fun comparteView(viewShare: View) {
+        // Creamos un bitmap con el tamaño de la vista
+        val bitmap = Bitmap.createBitmap(
+            viewShare.width,
+            viewShare.height, Bitmap.Config.ARGB_8888
+        )
+        // Creamos el canvas para pintar en el bitmap
+        val canvas = Canvas(bitmap)
+        // Pintamos el contenido de la vista en el canvas y así en el bitmap
+        viewShare.draw(canvas)
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+        var uriF: Uri? = null
+        try {
+            val f = File.createTempFile(
+                "sharedImage", ".jpg", externalCacheDir
+            )
+            f.deleteOnExit()
+            val fo = FileOutputStream(f)
+            fo.write(stream.toByteArray())
+            fo.close()
+            uriF = FileProvider.getUriForFile(
+                this,
+                this.applicationContext.packageName + ".provider",
+                f
+            );
+            val shareIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_STREAM, uriF)
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                type = "image/jpeg"
+            }
+
+            startActivity(
+                Intent.createChooser(
+                    shareIntent,
+                    resources.getText(R.string.send_to)
+                )
+            )
+
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
     }
 
 
